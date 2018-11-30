6283f6f980b0cea47abf5e170fdb34bb42cabcd2
tcrepy <tanguycrepy@gmail.com>
2018-11-30 11:48:09 +0100

    INIT git

---
+++ src/app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'mariadb',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'root',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'gpBdkexxsvK7Yc3wHL1yPIkKjDqmrdjwv523ySGrEseWGaMqzw8SgQI9',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-11-30',
+    'locale' => 'fr-FR',
+    'cookie_key' => 'HAWY5JZFGzLxKN7BhkkEm9LcEd7j1BbOAmm2uJ8bRD8mrnOn0i0WOamw',
+    'cookie_iv' => 'PUdOafGO',
+    'new_cookie_key' => 'def000007e087ee8f5e6473d9d6365cfc8dead1239c498c99ab86c7a291b532627c0826f15c0acf1a69326ca3c118c7e72cf9b2f618f249baaedda833efc86141a6bc4ae',
+  ),
+);
